32095cef82d4a120023f07d3b26f8f79ad4d819c
Kevin Dornellas <dornellaskj@gmail.com>
2021-02-10 20:02:46 -0700

    complete ch1 of Secure software design

--- src/app/services/secure-software-design.service.ts
+++ src/app/services/secure-software-design.service.ts
@@ -18,114 +18,84 @@ export class SecureSoftwareDesignService {
   getQuestions() {
     return [
       {
-        question:"aaa",
+        question:"Developers must take the time to code __________, and eradicate __________ security flaw before the code goes into production.",
         answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
+          "cleanly, every possible",
+          "quickly, the least possible", 
+          "efficiently, the most common", 
+          "slowly, the most prevalent"
         ],
         correct: 0
       },
       {
-        question:"aaa",
+        question:"What are two attributes which complement each other and enhance overall software product integrity and market value?",
         answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
+          "Proprietary, shared",
+          "Open source, closed source", 
+          "Reliability, usability", 
+          "Quality, security"
         ],
-        correct: 0
+        correct: 3
       },
       {
-        question:"aaa",
+        question:"__________ and __________ are the two properties that support confidentiality as one ensures users have the appropriate role and privilege to view data, and the other ensures users are who they claim to be and that the data come from the appropriate place.",
         answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
+          "Availability, authenticity",
+          "Access, authorization", 
+          "Authorization, authentication", 
+          "Asymmetry, access"
         ],
-        correct: 0
+        correct: 2
       },
       {
-        question:"aaa",
+        question:"What is responsible for preserving authorized restrictions on information access and disclosure, including means for protecting personal privacy and proprietary information?",
         answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
+          "Availability",
+          "Confidentiality", 
+          "Authorization", 
+          "Integrity"
         ],
-        correct: 0
+        correct: 1
       },
       {
-        question:"aaa",
+        question:"What is responsible for guarding against improper information modification or destruction, and includes ensuring information non-repudiation and authenticity?",
         answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
+          "Confidentiality",
+          "Integrity", 
+          "Availability", 
+          "Authorization"
         ],
-        correct: 0
+        correct: 1
       },
       {
-        question:"aaa",
+        question:"What is responsible for ensuring timely and reliable access to and use of information?",
         answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
+          "Confidentiality",
+          "Integrity", 
+          "Availability", 
+          "Authorization"
         ],
-        correct: 0
+        correct: 2
       },
       {
-        question:"aaa",
+        question:"Which concept in the software life cycle understands the potential security threats to the system, determines risk, and establishes appropriate mitigations?",
         answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
+          "Penetration testing",
+          "Vulnerability assessment", 
+          "Attack surface validation", 
+          "Threat modeling"
         ],
-        correct: 0
+        correct: 3
       },
       {
-        question:"aaa",
+        question:"The majority of __________ against software __________ some vulnerability or weakness in that software; these terms are often used interchangeably.",
         answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
+          "threats, hack",
+          "malware, flaw", 
+          "attacks, exploit", 
+          "mitigations, remediate"
         ],
-        correct: 0
-      },
-      {
-        question:"aaa",
-        answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
-        ],
-        correct: 0
-      },
-      {
-        question:"aaa",
-        answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
-        ],
-        correct: 0
-      },
-      {
-        question:"aaa",
-        answers: [
-          "aaa",
-          "aaa", 
-          "aaa", 
-          "aaa"
-        ],
-        correct: 0
+        correct: 2
       }
     ]
   }
