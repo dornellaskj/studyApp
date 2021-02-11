89153898a4b844cbe9bbd07788f5fd29a0b035f1
Kevin Dornellas <dornellaskj@gmail.com>
2021-02-10 20:09:33 -0700

    complete ch2 of Secure software design

--- src/app/services/secure-software-design.service.ts
+++ src/app/services/secure-software-design.service.ts
@@ -18,6 +18,86 @@ export class SecureSoftwareDesignService {
   getQuestions() {
     return [
       {
+        question:"The most well-known SDL model is the __________, a process that Microsoft has adopted for the development of software that needs to withstand malicious attack and is considered the most mature of the top three models.",
+        answers: [
+          "Cigital Software Security Touchpoints model",
+          "OWASP Security Development Lifecycle", 
+          "Cisco Secure Development Lifecycle", 
+          "Trustworthy Computing Security Development Lifecycle"
+        ],
+        correct: 3
+      },
+      {
+        question:"The ISO/IEC __________ standard provides guidance to help organizations embed security within their processes that help secure applications running in the environment, including application life cycle processes.",
+        answers: [
+          "17799",
+          "27034", 
+          "27001", 
+          "SDLC"
+        ],
+        correct: 1
+      },
+      {
+        question:"__________ is a global, industry-led effort to identify and promote best practices for developing and delivering more secure and reliable software, hardware, and services.",
+        answers: [
+          "NCSD",
+          "BSI", 
+          "SAFECode", 
+          "SDLC"
+        ],
+        correct: 2
+      },
+      {
+        question:"The __________ is a list of information security vulnerabilities and exposures that aims to provide common names for publicly known problems.",
+        answers: [
+          "CVE",
+          "SAMATE", 
+          "NCSD", 
+          "SDLC"
+        ],
+        correct: 0
+      },
+      {
+        question:"__________ is the analysis of computer software that is performed by executing programs on a real or virtual processor in real time.",
+        answers: [
+          "Fuzzing",
+          "Dynamic program analysis", 
+          "Static program analysis", 
+          "Scratching"
+        ],
+        correct: 1
+      },
+      {
+        question:"__________ is a black-box software testing technique that can be automated and provides invalid, unexpected, or random data to the inputs of a computer software program.",
+        answers: [
+          "Fuzzing",
+          "Dynamic program analysis", 
+          "Static program analysis", 
+          "Scratching"
+        ],
+        correct: 0
+      },
+      {
+        question:"__________ is the analysis of computer software that is performed without actually executing programs.",
+        answers: [
+          "Fuzzing",
+          "Dynamic program analysis", 
+          "Static program analysis", 
+          "Scratching"
+        ],
+        correct: 2
+      },
+      {
+        question:"The __________ requires that in a particular abstraction layer of a computing environment, every module must be able to access only the information and resources that are necessary for its legitimate purpose.",
+        answers: [
+          "principle of least privilege",
+          "software security maturity model", 
+          "secure development life cycle", 
+          "software assurance forum"
+        ],
+        correct: 0
+      },
+      {
         question:"Developers must take the time to code __________, and eradicate __________ security flaw before the code goes into production.",
         answers: [
           "cleanly, every possible",
