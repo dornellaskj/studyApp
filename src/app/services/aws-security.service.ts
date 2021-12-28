396da7b69cae8f0dc856658a037446ab22ac3067
Kevin Dornellas <dornellaskj@gmail.com>
2021-12-28 11:03:46 -0700

    added new IAM questions to AWS study

--- src/app/services/aws-security.service.ts
+++ src/app/services/aws-security.service.ts
@@ -115,7 +115,106 @@ export class AwsSecurityService {
         ],
         correct: 2
       },
-    
+      {
+          question:"For which of the following scenarios does AWS Cognito best fit?",
+          answers: [
+            "External users accessing AWS services by means of web application",
+            "SAML Federated Identity", 
+            "Multiple AWS accounts that need to share resources", 
+            "AWS account integrated with on-premises Active Directory"
+          ],
+          correct: 0
+        },
+        {
+          question:"As IT administrator of your company's AWS account, you are asked to ensure that all actions performed by people accessing AWS resources are tracked and the resulting logs are securely stored. Which combination of services would you use to satisfy the requirement?",
+          answers: [
+            "CloudTrail and EC2",
+            "CloudTrail and S3 Glacier", 
+            "CloudTrail and S3", 
+            "CloudTrail and CloudWatch"
+          ],
+          correct: 2
+        },
+        {
+          question:"Which of the following statements is true about IAM credential report?",
+          answers: [
+            "A credential report lists all users in your account and the status of their credentials.",
+            "A credential report is a tool that automatically provides remediation in case of a compromised password.", 
+            "A credential report only contains information related to AWS access keys and AWS secure access keys.", 
+            "A credential report is a tool to apply password policy to IAM users credentials."
+          ],
+          correct: 0
+        },
+        {
+          question:"Whats is true about AWS IAM policy evaluation?",
+          answers: [
+            "Implicit deny overrides explicit allow",
+            "Explicit deny overrides explicit allow", 
+            "Explicit allow is the default behavior", 
+            "Implicit allow is the default behavior"
+          ],
+          correct: 1
+        },
+        {
+          question:"What statement is true about CloudTrail?",
+          answers: [
+            "Monitored events include actions taken in the AWS Management Console, AWS Command Line Interface, and AWS SDKs and APIs.",
+            "Cloudtrail data events include datap-plane events that take place within DynamoDB, lambda, and S3.", 
+            "CloudTrail store logs as HTML files in EFS storage.", 
+            "CloudTrail is not able to track actions performed by means of the AWS dashboard."
+          ],
+          correct: 0
+        },
+        {
+          question:"Which of the following statements is true about IAM?",
+          answers: [
+            "It enables you to manage access to AWS services and resources",
+            "It enables authorization only", 
+            "It enables authorization and authentication for local users", 
+            "It enables authentication only"
+          ],
+          correct: 0
+        },
+        {
+          question:"Which of the following statements is true about IAM users and groups?",
+          answers: [
+            "Users and groups are global objects",
+            "Users and groups are local to a current AWS region", 
+            "Users and groups are local to a current AWS availability zone", 
+            "Users are local IAM objects, while groups are IAM global objects"
+          ],
+          correct: 0
+        },
+        {
+          question:"You are the Administrator of an AWS account. The CEO asks that everyone sign in to their AWS account with a strong authentication method. Which of the following would you implement?",
+          answers: [
+            "Enable MFA for all users that need access via the dashboard.",
+            "Roll out a custom solution. Enable MFA for the root user only.", 
+            "Enable MFA for all users.", 
+            "Enable MFA for all users that need access via programmatic access."
+          ],
+          correct: 2
+        },
+        {
+          question:"Which statement is true about IAM policy documents?",
+          answers: [
+            "Effect, action and resource are mandatory parameters",
+            "An IAM policy document must contain a single statement", 
+            "Effect, condition and resource are mandatory parameters", 
+            "An IAM policy document is written in XML format"
+          ],
+          correct: 2
+        },
+        {
+          question:"Which of the following statements is correct about AWS Cognito?",
+          answers: [
+            "Identity pools are user directories, while user pools enable you to grant your users access to other AWS services",
+            "Identity pools are populated with users authenticated by external Identity Providers.", 
+            "A fully working AWSCognito instance consists of a user pool and an identity pool.", 
+            "User pools are user directories, while identity pools enable you to grant your users access to other AWS services."
+          ],
+          correct: 3
+        },
     ];
   }
 }
