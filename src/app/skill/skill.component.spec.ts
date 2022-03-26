1afc954082ccfb2ca31f5df5f61d5af9f712e5ae
Kevin D'Ornellas <dornellaskj@gmail.com>
2022-03-26 11:13:49 -0600

    tests passing, update deploy

--- src/app/skill/skill.component.spec.ts
+++ src/app/skill/skill.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { SkillComponent } from './skill.component';
 
@@ -16,10 +17,22 @@ describe('SkillComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SkillComponent);
     component = fixture.componentInstance;
+    component.skill =
+      {
+          "label": "Agile Development",
+          "power": 100,
+          "message": "My teams give accurate estimates and continuously improve every iteration.",
+          "tooltip": "Expert"
+      };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the correct label', () => {
+    let label = fixture.debugElement.query(By.css('.skill-label'));
+    expect(label.nativeElement.textContent).toBe('Agile Development');
   });
 });
