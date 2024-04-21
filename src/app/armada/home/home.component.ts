6ed942e048476d3e290eb561691d6fc6222e8961
kevin dornellas <dornellaskj@gmail.com>
2024-04-21 11:04:53 -0600

    app is running, google login installed

--- src/app/armada/home/home.component.ts
+++ src/app/armada/home/home.component.ts
@@ -1,4 +1,4 @@
-//import { SocialUser, SocialAuthService, GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { SocialUser, SocialAuthService, GoogleLoginProvider } from '@abacritt/angularx-social-login';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,24 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  // user: SocialUser | null; 
+  user: SocialUser | null; 
 
-  // constructor(private authService: SocialAuthService) 
-  // { 
-	// this.user = null;
-	// this.authService.authState.subscribe((user: SocialUser) => {
-	//   console.log(user);
-	//   this.user = user;
-	// });
-  // }
+  constructor(private authService: SocialAuthService) 
+  { 
+    this.user = null;
+    this.authService.authState.subscribe((user: SocialUser) => {
+      console.log(user);
+      this.user = user;
+      localStorage.setItem('username', user.name);
+      localStorage.setItem('googleId', user.idToken);
+      localStorage.setItem('userEmail', user.email);
+    });
+  }
 
-  // signInWithGoogle(): void {
-  //   this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then((x: any) => console.log(x));
-  // }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then((x: any) => 
+      {
+        localStorage.setItem('username', x.name);
+        localStorage.setItem('googleId', x.idToken);
+        localStorage.setItem('userEmail', x.email);
+      });
+  }
 
-  // signOut(): void {
-  //   this.authService.signOut();
-  // }  
+  isAlowedWrite() {
+    if (this.user.email == 'dornellaskj@gmail.com') {
+      return true;
+    }
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }  
 
   ngOnInit(): void {
   }
